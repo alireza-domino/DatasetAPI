b06112bde040a5cb9c841acf6013bdd1c5a76178
Alireza Mounesisohi <alireza.mounesisohi@dominodatalab.com>
2022-03-14 15:54:57 -0700

    Pushed from Domino: https://fldfive8383.cs.domino.tech/workspace/ddl-alireza/datasetAPI?executionId=622be4704b34ac6710742937

---
+++ .ipynb_checkpoints/S3 Domino job-checkpoint.py
@@ -0,0 +1,37 @@
+# run as a job
+# download a file through data source
+# daily report
+
+
+from domino import data_sources
+import os
+
+
+myClient = data_sources.DataSourceClient()
+myClient
+
+
+DATA_SOURCE_NAME = "ali-test-2-S3"
+data_source_obj = myClient.get_datasource(DATA_SOURCE_NAME)
+
+
+print(data_source_obj)
+
+
+# Key object
+file_name = "iris.csv"
+
+my_key = data_source_obj.Object(file_name)
+print(my_key)
+
+# GEt content as binary
+content = my_key.get()
+print("content binary")
+print(content)
+
+# Downlaoding 
+data_source_obj.Object(file_name).download_file(file_name)
+
+
+
+
